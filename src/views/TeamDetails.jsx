84ed7ae2e5dfce4103666ca60988b616523419d7
Dessie <desislava.m.alexandrova@gmail.com>
2025-08-02 15:37:57 +0300

    Visualizing a list of team players

--- src/views/TeamDetails.jsx
+++ src/views/TeamDetails.jsx
@@ -1,14 +1,18 @@
 import { useContext } from "react"
 import { DataContext } from "../context/DataContext"
+import { useParams } from "react-router-dom";
 
 export default function TeamDetails() {
 
+    const { id } = useParams();
     const { players } = useContext(DataContext);
+
+    const teamPlayers = players.filter((player) => player.TeamID == id)
 
     return (
         <>
            <ul>
-               {players.map((player) => {
+               {teamPlayers.map((player) => {
 
                     return (
                         <li key={player.ID}>
